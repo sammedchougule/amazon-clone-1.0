2014ad667045b4e7321ad807b29646699dc49ad7
Sammed Chougule <51811899+sammedchougule@users.noreply.github.com>
2022-04-14 23:20:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,11 @@
-
-
 var firebaseConfig = {
-    apiKey: "AIzaSyC4YNddVg4HcEWM64f-SWg4ld8N3PZfoAk",
-    authDomain: "clone-7b85f.firebaseapp.com",
-    projectId: "clone-7b85f",
-    storageBucket: "clone-7b85f.appspot.com",
-    messagingSenderId: "827305997254",
-    appId: "1:827305997254:web:70675e70c54c1067a178c7",
-    measurementId: "G-S171PEY5EY"
+    apiKey: ",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   // Initialize Firebase
